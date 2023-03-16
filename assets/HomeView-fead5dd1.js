522d7b0ad1d593d4640b60f0aa9427d4688130a7
sakimotorin <sakimotorin@gmail.com>
2023-03-16 14:36:12 +0800

    deploy backend page - add upload image function

---
+++ assets/HomeView-fead5dd1.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./index-e02089fa.js";const c={},s={class:"text-center text-danger"};function n(r,a,_,p,d,f){return o(),t("h2",s,"Home")}const m=e(c,[["render",n]]);export{m as default};
