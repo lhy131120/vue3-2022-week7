96b57baa6e259e17f1733a4063f848b74e06d9c1
sakimotorin <sakimotorin@gmail.com>
2023-03-16 14:35:35 +0800

    update vite.config.js the base to /vue3-2022-week7

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
     })
   ],
-  base: '/vue3-2022-week6/',
+  base: '/vue3-2022-week7/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
