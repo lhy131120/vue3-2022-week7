f8e6d2f77fee39723d2b43172c081154b9d7439e
sakimotorin <sakimotorin@gmail.com>
2023-03-16 14:31:47 +0800

    deploy backend page - add upload image function

---
+++ assets/AboutView-ce0815d5.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o}from"./index-77d831c0.js";const c={},s={class:"text-center text-danger"};function n(r,a,_,p,d,f){return o(),e("h2",s,"About")}const u=t(c,[["render",n]]);export{u as default};
---
+++ assets/HomeView-0ced0b83.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./index-77d831c0.js";const c={},s={class:"text-center text-danger"};function n(r,a,_,p,d,f){return o(),t("h2",s,"Home")}const m=e(c,[["render",n]]);export{m as default};
